3fdfac5ba466327c9859510d638c2ee3b10c10cb
William Oliveira <will_belo1@hotmail.com>
2024-06-04 16:18:01 -0300

    teste

--- routes/api.php
+++ routes/api.php
@@ -10,4 +10,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/signin', [UserController::class, 'signin']);
 Route::post('/register', [UserController::class, 'create']);
-Route::get('/verify', [UserController::class, 'verify'])->middleware(EnsureTokenIsValid::class);
+Route::get('/verif', [UserController::class, 'verify'])->middleware(EnsureTokenIsValid::class);
