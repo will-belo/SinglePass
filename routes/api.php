c120979324820f1477da2f6cc51ff4911487ce00
William Oliveira <will_belo1@hotmail.com>
2024-06-18 15:08:46 -0300

    Recuperação

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\EnsureTokenIsValid;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
@@ -9,5 +10,11 @@ use Illuminate\Support\Facades\Route;
 // })->middleware('auth:sanctum');
 
 Route::post('/signin', [UserController::class, 'signin']);
+
 Route::post('/register', [UserController::class, 'create']);
+
 Route::get('/verify', [UserController::class, 'verify'])->middleware(EnsureTokenIsValid::class);
+
+Route::get('/bcrypt', function () {
+    return response()->json(Hash::make('162867'));
+});
